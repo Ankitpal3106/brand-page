5fc54ee80997e055aafccebfe7a1e29a2ace9b6b
Ankit <ankitpal3106@gmail.com>
2023-08-13 16:43:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Hero from './Components/Hero'
+import Navigation from './Components/Navigation'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Navigation/>
+      <Hero/>
+     
+    </>
+  )
+}
+
+export default App
